56f44564b95ac04aa88cbf374296a90ad694b0a1
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-05-28 15:06:33 +0900

    ユーザー認証追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#me'
+  get 'home/index'
+  get 'login', 'sessions/create'
+  get 'logout', 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only:%i[new create]
 end
