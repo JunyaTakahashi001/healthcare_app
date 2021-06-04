8cfda9cee4c9fc20d28eaab0f428dfabdebc8a9f
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-06-05 08:50:23 +0900

    healthモデルに一意性バリデーション追加

--- app/models/health.rb
+++ app/models/health.rb
@@ -1,4 +1,5 @@
 class Health < ApplicationRecord
     validates :temperature_morning, presence: true, length: { maximum: 4 }
     validates :temperature_night, presence: true, length: { maximum: 4 }
+    validates :user_id, uniqueness: { scope: [:date, :user_id] }
 end
