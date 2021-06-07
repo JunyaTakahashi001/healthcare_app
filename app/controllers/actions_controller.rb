e8f20eee10d26717de368a494477237916c3df4e
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-06-07 20:01:58 +0900

    actionモデル名変更前

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -10,6 +10,23 @@ class ActionsController < ApplicationController
     @action = Action.new(date: @params)
   end
 
+  def create
+    action = Action.new(action_params)
+    if action.save
+      flash[:notice] = "「#{action.date}」の行動記録を登録しました"
+      redirect_to healths_path
+    else
+      flash[:error_messages] = action.errors.full_messages
+      redirect_back(fallback_location: action_new_path)
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def action_params
+    params.require(:action).permit(:date, :serial_num, :start_time, :end_time, :destination, :transportation, :departure, :arrival, :attendees, :attendees_details, :other, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'actions/index'
-  get 'actions/show'
-  get 'actions/new', to: 'actions#new'
-  get 'actions/edit'
-  
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'actions/new', to: 'actions#new'
+  post 'actions', to: 'actions#create'
   post 'healths', to: 'healths#create'
   resources :users, only:%i[new create]
   resources :healths
+  resources :actions
 end
