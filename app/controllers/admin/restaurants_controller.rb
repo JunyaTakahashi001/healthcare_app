e16522f98fd475a0522febcf8d96e64f2d1682ef
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-06-10 10:09:47 +0900

    個人別一覧作成前

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,14 +1,20 @@
 class Admin::RestaurantsController < ApplicationController
   before_action :if_not_admin
-	before_action :set_restaurant, only: [:show, :edit, :destroy]
+	before_action :set_restaurant, only: [ :edit, :destroy]
 
   def index
 		# クエリストリングがあればTimeオブジェクトに変換、ない場合は現在の時刻を取得
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     # 取得した時刻が含まれる日の範囲のデータを取得
-    # @healths = Health.joins(:user).where(date: @month.all_day).order('date ASC')
     @healths = Health.left_joins(:user).select('healths.*,users.name').where(date: @month.all_day).order('date ASC')
 	end
+
+  def show
+    # クエリストリングがあればTimeオブジェクトに変換、ない場合は現在の時刻を取得
+    @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
+    # 取得した時刻が含まれる日の範囲のデータを取得
+    @actionlogs = Actionlog.where(date: @month.all_day).where(id: @user_id).order('date ASC')  
+  end
 
   private
   def if_not_admin
